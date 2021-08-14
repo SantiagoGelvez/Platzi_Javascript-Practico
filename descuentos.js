60807634826dd2eb1db639ea4c6a764a780d480b
Santiago Andres Gelvez Camargo <santiagogelvez29@gmail.com>
2021-08-14 17:23:36 -0500

    Finaliza la primera versión de descuentos. Se cambia el boton por el evento onkeyup

--- descuentos.js
+++ descuentos.js
@@ -1,16 +1,25 @@
-// const precioOriginal = 120;
-// const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
 
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento
-// })
-// console.log(`El precio original es ${precioOriginal} dolares`);
+// function onClickButtonPriceDiscount() {
+//     const inputPrice = document.getElementById("InputPrice");
+//     const inputDiscount = document.getElementById("InputDiscount");
+//     const priceValue = inputPrice.value;
+//     const discountValue = inputDiscount.value;
+//     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+//     const resultPrice = document.getElementById("ResultPrice");
+//     resultPrice.innerText = "El precio con descuento es: $" + precioConDescuento
+// }
+
+function onKeyUpDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const inputDiscount = document.getElementById("InputDiscount");
+    const priceValue = inputPrice.value;
+    const discountValue = inputDiscount.value;
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const resultPrice = document.getElementById("ResultPrice");
+    resultPrice.innerText = "$" + new Intl.NumberFormat('es-CO').format(precioConDescuento);
+}
