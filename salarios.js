e1a3cea84ac90435cd1189da95f4b5ef640687f0
Santiago Gelvez <santiagogelvez29@gmail.com>
2021-10-22 19:27:42 -0500

    Ejercicio llamado analisis salarial del curso e Platzi

---
+++ salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+    name: "Erika",
+    salary: 5000000
+});
+colombia.push({
+    name: "Jose",
+    salary: 2400000
+});
+colombia.push({
+    name: "Duvan",
+    salary: 1500000
+});
+colombia.push({
+    name: "Mario",
+    salary: 3200000
+});
+colombia.push({
+    name: "Camilo",
+    salary: 5300000
+});
+colombia.push({
+    name: "Yesenia",
+    salary: 1900000
+});
+colombia.push({
+    name: "Laura",
+    salary: 2700000
+});
+colombia.push({
+    name: "Kelly",
+    salary: 2600000
+});
+colombia.push({
+    name: "Anderson",
+    salary: 2600000
+});
+colombia.push({
+    name: "Angel",
+    salary: 3000000
+});
+colombia.push({
+    name: "Javier",
+    salary: 1000000
+});
+colombia.push({
+    name: "Daniela",
+    salary: 1000000
+});
+colombia.push({
+    name: "Jimena",
+    salary: 4200000
+});
+colombia.push({
+    name: "Catalina",
+    salary: 7300000
+});
+colombia.push({
+    name: "Dario",
+    salary: 2000000
+});
+colombia.push({
+    name: "Sebastian",
+    salary: 2100000
+});
+colombia.push({
+    name: "Norberto",
+    salary: 2300000
+});
+colombia.push({
+    name: "Margarita",
+    salary: 3800000
+});
+colombia.push({
+    name: "Viviana",
+    salary: 2700000
+});
+colombia.push({
+    name: "Santiago",
+    salary: 2800000
+});
